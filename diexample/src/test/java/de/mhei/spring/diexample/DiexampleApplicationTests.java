f05ed654f3768b72f391e183418f339a0c2d9be6
Marcus Heisters <marcus.heisters@googlemail.com>
2023-10-07 18:05:24 +0200

    'initial commit

---
+++ diexample/src/test/java/de/mhei/spring/diexample/DiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mhei.spring.diexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
